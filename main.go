6e27635becfa52f43a9b263eb38c4edabc9386c6
felipecruz91 <felipecruz91@hotmail.es>
2023-03-01 23:58:56 +0100

    Compute total number of vulnerabilities by severity

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"flag"
 	"log"
@@ -72,14 +73,132 @@ func main() {
 		log.Fatal(err)
 	}
 
+	criticalVuln := 0
+	highVuln := 0
+	mediumVuln := 0
+	lowVuln := 0
+	unspecifiedVuln := 0
+
 	for _, image := range images {
-		outputFile := filepath.Join("results", regexp.MustCompile(`[^a-zA-Z	0-9]+`).ReplaceAllString(image, "_")+".sarif.json")
+		// replace the matched non-alphanumeric characters with the underscore character
+		outputFile := filepath.Join("results", regexp.MustCompile(`[^a-zA-Z-0-9]+`).ReplaceAllString(image, "_")+".sarif.json")
 		cmd := exec.Command("docker", "scout", "cves", "--format", "sarif", "--only-fixed", "--output", outputFile, image)
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
 		if err := cmd.Run(); err != nil {
 			log.Fatal(err)
 		}
+
+		b, err := os.ReadFile(outputFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		var report SarifReport
+
+		if err := json.Unmarshal(b, &report); err != nil {
+			log.Fatal(err)
+		}
+
+		for _, result := range report.Runs[0].Results {
+
+			for _, rule := range report.Runs[0].Tool.Driver.Rules {
+				if rule.ID == result.RuleID {
+					log.Printf("Severity: %s", rule.Properties.CvssV3Severity)
+
+					switch rule.Properties.CvssV3Severity {
+					case "UNSPECIFIED":
+						unspecifiedVuln += 1
+					case "LOW":
+						lowVuln += 1
+					case "MEDIUM":
+						mediumVuln += 1
+					case "HIGH":
+						highVuln += 1
+					case "CRITICAL":
+						criticalVuln += 1
+					}
+					break
+				}
+			}
+
+		}
+
 	}
 
+	log.Printf("Total critical: %d", criticalVuln)
+	log.Printf("Total high: %d", highVuln)
+	log.Printf("Total medium: %d", mediumVuln)
+	log.Printf("Total low: %d", lowVuln)
+	log.Printf("Total unspecified: %d", unspecifiedVuln)
 }
+
+type SarifReport struct {
+	Version string `json:"version"`
+	Schema  string `json:"$schema"`
+	Runs    []struct {
+		Tool struct {
+			Driver struct {
+				FullName       string `json:"fullName"`
+				InformationURI string `json:"informationUri"`
+				Name           string `json:"name"`
+				Rules          []struct {
+					ID               string `json:"id"`
+					Name             string `json:"name"`
+					ShortDescription struct {
+						Text string `json:"text"`
+					} `json:"shortDescription"`
+					HelpURI string `json:"helpUri"`
+					Help    struct {
+						Text     string `json:"text"`
+						Markdown string `json:"markdown"`
+					} `json:"help"`
+					Properties struct {
+						AffectedVersion string   `json:"affected_version"`
+						CvssV3Severity  string   `json:"cvssV3_severity"`
+						FixedVersion    string   `json:"fixed_version"`
+						Tags            []string `json:"tags"`
+					} `json:"properties,omitempty"`
+				} `json:"rules"`
+				Version string `json:"version"`
+			} `json:"driver"`
+		} `json:"tool"`
+		Results []struct {
+			RuleID    string `json:"ruleId"`
+			RuleIndex int    `json:"ruleIndex"`
+			Kind      string `json:"kind"`
+			Level     string `json:"level"`
+			Message   struct {
+				Text string `json:"text"`
+			} `json:"message"`
+			Locations []struct {
+				LogicalLocations []struct {
+					Name               string `json:"name,omitempty"`
+					FullyQualifiedName string `json:"fullyQualifiedName"`
+					Kind               string `json:"kind,omitempty"`
+				} `json:"logicalLocations"`
+			} `json:"locations"`
+		} `json:"results"`
+	} `json:"runs"`
+}
+
+//
+//type SarifReport struct {
+//	Runs []struct {
+//		Results []struct {
+//			RuleID    string `json:"ruleId"`
+//			RuleIndex int    `json:"ruleIndex"`
+//			Kind      string `json:"kind"`
+//			Level     string `json:"level"`
+//			Message   struct {
+//				Text string `json:"text"`
+//			} `json:"message"`
+//			Locations []struct {
+//				LogicalLocations []struct {
+//					Name string `json:"name,omitempty"`
+//					FQN  string `json:"fullyQualifiedName"`
+//					Kind string `json:"kind,omitempty"`
+//				} `json:"logicalLocations"`
+//			} `json:"locations"`
+//		} `json:"results"`
+//	} `json:"runs"`
+//}
